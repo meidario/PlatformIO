9a5f8ee303357163ef181967f21706eecde7b6fc
Dario Meier <dario.meier94@gmx.ch>
2025-05-18 18:32:21 +0200

    First Try Ampel

--- LE2_Ampel_Uebung5/src/main.cpp
+++ LE2_Ampel_Uebung5/src/main.cpp
@@ -19,6 +19,7 @@ Projekt 5 - Eine Verkehrsampel
 boolean trafficWest = true; // West = true, Ost = false
 int flowTime = 4000; // Zeitraum, in dem der Verkehr fliesst
 int changeDelay = 2000; // Zeitraum zwischen Farbwechseln
+int step = 0;
 void setup() {
 // Richtet die digitalen E/A-Pins ein
 pinMode(westButton, INPUT);
@@ -38,4 +39,103 @@ digitalWrite(eastYellow, LOW);
 digitalWrite(eastGreen, LOW);
 }
 void loop() {
+
+    if (digitalRead(eastButton) == HIGH and (digitalRead(trafficWest)) == HIGH){
+        /* code */
+        switch (step)
+        {
+        case 0: //West to yellow
+            /* code */
+            digitalWrite(westYellow, HIGH);
+            delay(changeDelay);
+            step = 1;
+            break;
+
+        case 1: //West to red
+            /* code */
+            digitalWrite(westYellow, LOW);
+            digitalWrite(westRed, HIGH);
+            delay(changeDelay);
+            step = 2;
+            break;
+        case 2: //Wait for traffic
+            /* code */
+            delay(flowTime);
+            step = 3;
+            break;
+        case 3: //blink yellow east
+            /* code */
+            digitalWrite(eastYellow, HIGH);
+            delay(yellowBlinkTime);
+            digitalWrite(eastYellow, LOW);
+            delay(yellowBlinkTime);
+            delay(changeDelay);
+            step = 4;
+            break;
+        case 4: //East to green
+            /* code */
+            digitalWrite(eastRed, LOW);
+            digitalWrite(eastGreen, HIGH);
+            trafficWest = LOW;
+            step = 0;
+            break;
+        
+        default:
+            break;
+        }
+    }
+    else if (digitalRead(westButton) == HIGH and (digitalRead(trafficWest)) == LOW){
+        /* code */
+        switch (step)
+        {
+        case 0: //East to yellow
+            /* code */
+            digitalWrite(eastYellow, HIGH);
+            delay(changeDelay);
+            step = 1;
+            break;
+
+        case 1: //East to red
+            /* code */
+            digitalWrite(eastYellow, LOW);
+            digitalWrite(eastRed, HIGH);
+            delay(changeDelay);
+            step = 2;
+            break;
+        case 2: //Wait for traffic
+            /* code */
+            delay(flowTime);
+            step = 3;
+            break;
+        case 3: //blink yellow west
+            /* code */
+            digitalWrite(westYellow, HIGH);
+            delay(yellowBlinkTime);
+            digitalWrite(westYellow, LOW);
+            delay(yellowBlinkTime);
+            delay(changeDelay);
+            step = 4;
+            break;
+        case 4: //West to green
+            /* code */
+            digitalWrite(westRed, LOW);
+            digitalWrite(westGreen, HIGH);
+            trafficWest = HIGH;
+            step = 0;
+            break;
+        }
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
