d93c01ef81f3d8bb6444a5caf69b5ae9c10de8f9
Dario Meier <dario.meier94@gmx.ch>
2025-05-18 22:27:49 +0200

    Ampel tested, add button triggers and resetting variables

--- LE2_Ampel_Uebung5/src/main.cpp
+++ LE2_Ampel_Uebung5/src/main.cpp
@@ -4,24 +4,29 @@
 Traffic Light
 Projekt 5 - Eine Verkehrsampel
 */
-// nur bei Sloeber IDE
-#include "Arduino.h"
+
 // Definiert die Pins, mit denen die Tasten und LEDs verbunden sind:
-#define westButton 3
+#define westButton 5
 #define eastButton 13
-#define westRed 2
-#define westYellow 1
-#define westGreen 0
+#define westRed 4
+#define westYellow 3
+#define westGreen 2
 #define eastRed 12
 #define eastYellow 11
 #define eastGreen 10
 #define yellowBlinkTime 500 // Lässt die gelbe LED 0,5 s blinken
-boolean trafficWest = true; // West = true, Ost = false
 int flowTime = 4000; // Zeitraum, in dem der Verkehr fliesst
 int changeDelay = 2000; // Zeitraum zwischen Farbwechseln
 int step = 0;
-void setup() {
+
+// --- Edge Detection ---
+boolean trafficWest = true; // West = true, Ost = false
+boolean lastEastButtonState = false;
+boolean lastWestButtonState = false;
+boolean eastRequest = false;      // Trigger from east
+boolean westRequest = false;      // Trigger from west
 // Richtet die digitalen E/A-Pins ein
+void setup() {
 pinMode(westButton, INPUT);
 pinMode(eastButton, INPUT);
 pinMode(westRed, OUTPUT);
@@ -39,8 +44,22 @@ digitalWrite(eastYellow, LOW);
 digitalWrite(eastGreen, LOW);
 }
 void loop() {
+  // ---- Detect Button Triggers ----
+  bool currentEast = digitalRead(eastButton);
+  bool currentWest = digitalRead(westButton);
 
-    if (digitalRead(eastButton) == HIGH and (digitalRead(trafficWest)) == HIGH){
+  if (currentEast == HIGH and lastEastButtonState == LOW and trafficWest and (step == 0 or step == -1)) {
+    eastRequest = true;
+    westRequest = false;
+  }
+  if (currentWest == HIGH and lastWestButtonState == LOW and not trafficWest and (step == 0 or step == -1)) {
+    westRequest = true;
+    eastRequest = false;
+  }
+
+  lastEastButtonState = currentEast;
+  lastWestButtonState = currentWest;
+    if (eastRequest){
         /* code */
         switch (step)
         {
@@ -53,6 +72,7 @@ void loop() {
 
         case 1: //West to red
             /* code */
+            digitalWrite(westGreen, LOW);
             digitalWrite(westYellow, LOW);
             digitalWrite(westRed, HIGH);
             delay(changeDelay);
@@ -65,26 +85,28 @@ void loop() {
             break;
         case 3: //blink yellow east
             /* code */
+        for (int i = 0; i < changeDelay / (2 * yellowBlinkTime); i++) {
             digitalWrite(eastYellow, HIGH);
             delay(yellowBlinkTime);
             digitalWrite(eastYellow, LOW);
             delay(yellowBlinkTime);
-            delay(changeDelay);
+            }
             step = 4;
             break;
         case 4: //East to green
             /* code */
             digitalWrite(eastRed, LOW);
             digitalWrite(eastGreen, HIGH);
-            trafficWest = LOW;
-            step = 0;
+            trafficWest = false;
+            eastRequest = false;
+            step = -1; // idle state until west button pressed
             break;
-        
+
         default:
             break;
         }
     }
-    else if (digitalRead(westButton) == HIGH and (digitalRead(trafficWest)) == LOW){
+    else if (westRequest){
         /* code */
         switch (step)
         {
@@ -97,6 +119,7 @@ void loop() {
 
         case 1: //East to red
             /* code */
+            digitalWrite(eastGreen, LOW);
             digitalWrite(eastYellow, LOW);
             digitalWrite(eastRed, HIGH);
             delay(changeDelay);
@@ -109,24 +132,29 @@ void loop() {
             break;
         case 3: //blink yellow west
             /* code */
+        for (int i = 0; i < changeDelay / (2 * yellowBlinkTime); i++) {
             digitalWrite(westYellow, HIGH);
             delay(yellowBlinkTime);
             digitalWrite(westYellow, LOW);
             delay(yellowBlinkTime);
-            delay(changeDelay);
-            step = 4;
+             }
+            step = 4;   
             break;
         case 4: //West to green
             /* code */
             digitalWrite(westRed, LOW);
             digitalWrite(westGreen, HIGH);
-            trafficWest = HIGH;
-            step = 0;
+            trafficWest = true;
+            westRequest = false;
+            step = -1; // idle state until west button pressed
             break;
         }
     }
 
-
+  // Reset to idle if no request but light is green
+  if (step == -1 and not eastRequest and not westRequest) {
+    step = 0;
+  }
 }
 
 
