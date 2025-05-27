439b60635bedffa5c881de3e256ff6406c3208ec
Dario Meier <dario.meier94@gmx.ch>
2025-05-27 19:50:07 +0200

    changes

--- LE4_Schieberegister/src/main.cpp
+++ LE4_Schieberegister/src/main.cpp
@@ -1,18 +1,22 @@
-#include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#include "Arduino.h"
+// Projekt 16: Eine Binärzahlenanzeige aus LEDs bauen
+#define DATA 6 // Digitalpin 6 an Pin 14 des 74HC595
+#define LATCH 8 // Digitalpin 8 an Pin 12 des 74HC595
+#define CLOCK 10 // Digitalpin 10 an Pin 11 des 74HC595
+void setup()
+{
+ pinMode(LATCH, OUTPUT);
+ pinMode(CLOCK, OUTPUT);
+ pinMode(DATA, OUTPUT);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+ int i;
+ for ( i = 0; i < 256; i++ )
+ {
+ digitalWrite(LATCH, LOW);
+ shiftOut(DATA, CLOCK, MSBFIRST, i);
+ digitalWrite(LATCH, HIGH);
+ delay(200);
+ }
 }
